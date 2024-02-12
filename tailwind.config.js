9d282dec6f513954f6bbd42a3eefb57ba61adbae
Michael Ehme <mc.ehme@gmail.com>
2024-02-11 22:44:43 -0500

    minifying css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/main/resources/templates/*.html","./src/main/resources/static/*.js"],
+  content: ["./src/main/resources/static/*.html","./src/main/resources/static/*.js"],
   theme: {
     extend: {},
   },
