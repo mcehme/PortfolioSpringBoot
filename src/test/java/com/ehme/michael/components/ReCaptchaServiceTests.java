ae05292a5944c1a3fce1d8714fc6261ec080de8f
Michael Ehme <mc.ehme@gmail.com>
2024-05-23 18:22:59 -0400

    attempting to debug GitHub actions

--- src/test/java/com/ehme/michael/components/ReCaptchaServiceTests.java
+++ src/test/java/com/ehme/michael/components/ReCaptchaServiceTests.java
@@ -50,7 +50,8 @@ public class ReCaptchaServiceTests {
         String json = new ObjectMapper().writeValueAsString(response);
 
         mockRestServiceServer.expect(MockRestRequestMatchers.requestTo(Matchers.containsStringIgnoringCase(VERIFY_URI))).andExpect(MockRestRequestMatchers.method(HttpMethod.POST)).andRespond(MockRestResponseCreators.withSuccess(json, MediaType.APPLICATION_JSON));
-//        boolean result = reCaptchaService.validate(new ReCaptchaToken("",""));
+        ReCaptchaToken reCaptchaToken = new ReCaptchaToken("","");
+        boolean result = reCaptchaService.validate(null);
         Assertions.assertTrue(true);
     }
 
