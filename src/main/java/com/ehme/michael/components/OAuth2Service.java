a2b008a79b81e8c029fc1c8ad74c192492df0cbe
Michael Ehme <mc.ehme@gmail.com>
2024-06-08 12:04:07 -0400

    Removing unused import

--- src/main/java/com/ehme/michael/components/OAuth2Service.java
+++ src/main/java/com/ehme/michael/components/OAuth2Service.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 import java.util.Map;
-import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
